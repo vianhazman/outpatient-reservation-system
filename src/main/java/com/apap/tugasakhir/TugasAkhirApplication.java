0d41ae6a5c2b4332df493a83a39bb3473ff65335
vianhazman <vianhazman@gmail.com>
2018-11-12 11:57:32 +0700

    project boilerplate

---
+++ src/main/java/com/apap/tugasakhir/TugasAkhirApplication.java
@@ -0,0 +1,12 @@
+package com.apap.tugasakhir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TugasAkhirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TugasAkhirApplication.class, args);
+	}
+}
