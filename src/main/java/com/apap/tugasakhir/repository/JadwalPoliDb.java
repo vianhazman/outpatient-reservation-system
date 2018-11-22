331f312c87cf6f525fb7497692bb87c17ba30b77
mona.natasha <mona_natasha@ymail.com>
2018-11-22 13:01:53 +0700

    commit

--- src/main/java/com/apap/tugasakhir/repository/JadwalPoliDb.java
+++ src/main/java/com/apap/tugasakhir/repository/JadwalPoliDb.java
@@ -1,5 +1,6 @@
 package com.apap.tugasakhir.repository;
 
+import java.sql.Date;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -7,7 +8,7 @@ import org.springframework.stereotype.Repository;
 import com.apap.tugasakhir.model.JadwalPoliModel;
 
 @Repository
-public interface JadwalPoliDb extends JpaRepository<JadwalPoliModel, Long>{
+public interface JadwalPoliDB extends JpaRepository<JadwalPoliModel, Long> {
+	JadwalPoliModel findByTanggalAndDokter(Date tanggal, long dokter);
 	
-
 }
