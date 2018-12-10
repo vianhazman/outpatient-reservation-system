26070cfb4d96468a4d595689ebc853b5b3c770c3
muhammaderlangga51 <muhammad.erlangga51@ui.ac.id>
2018-12-10 09:49:55 +0700

    pull master

--- src/main/java/com/apap/tugasakhir/repository/RujukanRawatJalanDB.java
+++ src/main/java/com/apap/tugasakhir/repository/RujukanRawatJalanDB.java
@@ -1,10 +1,16 @@
 package com.apap.tugasakhir.repository;
 
-import com.apap.tugasakhir.model.RujukanRawatJalanModel;
+import java.util.Date;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface RujukanRawatJalanDB extends JpaRepository<RujukanRawatJalanModel, Long>{
+import com.apap.tugasakhir.model.PasienModel;
+import com.apap.tugasakhir.model.RujukanRawatJalanModel;
 
+@Repository
+public interface RujukanRawatJalanDb extends JpaRepository<RujukanRawatJalanModel, Long> {
+	RujukanRawatJalanModel findByidPasienAndTanggalRujuk(long idPasien, Date tanggalRujuk);
+	RujukanRawatJalanModel findByidPasien(long idPasien);
+	RujukanRawatJalanModel findByTanggalRujuk(Date tanggalRujuk);
 }
